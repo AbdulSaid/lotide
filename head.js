c33f6151e46ee60da5065f50266ad053622b4de3
Abdul Said <abdulrahman.asaid@gmail.com>
2021-10-26 00:10:56 +0000

    head.js

---
+++ head.js
@@ -0,0 +1,17 @@
+const head = function(array) {
+  return array[0];
+}
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(firstArray, element) {
+  if (firstArray === element) {
+    console.log(`✅✅✅ Assertion Passed: ${firstArray} === ${element}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${firstArray} === ${element}`);
+  }
+};
+
+
+
+
+assertEqual(head([5]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hjello");
