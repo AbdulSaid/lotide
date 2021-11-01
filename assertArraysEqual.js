3e2f81c4600094c80107d5b9b4c353df899a1126
Abdul Said <abdulrahman.asaid@gmail.com>
2021-11-01 23:48:25 +0000

    added assertArray

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if(array1.length !== array2.length){
-    return false;
-  }
-  for (let x = 0; x < array1.length; x++) {
-    if (array1[x] !== array2[x]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays')
 
   
 /* Implement assertArraysEqual which will take in two arrays and console.log an appropriate message
@@ -16,12 +6,12 @@ to the console */
 const assertArraysEqual = function(actual, expected) {
   // if the arrays executed in eqArrays function come back as true, then it would use the Passed console.log
   if (eqArrays(actual, expected)) {
-    console.log(`✅ Assertion Passed: ${actual}  === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
     // Otherwise, it will say fail
   } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 
-assertArraysEqual([1, 2,3], [1, 2, 3]);
+module.exports= assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,4 @@
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+//Test function
+assertArraysEqual([1, 2,3], [1, 2, 3]);
