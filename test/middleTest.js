f1b76c2ec9622b2b8d5b5964ffcfc76cae279701
Abdul Said <abdulrahman.asaid@gmail.com>
2021-11-02 00:40:39 +0000

    added mocha for middle test and tail test

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,16 @@
 const eqArrays = require('../eqArrays');
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const { assert } = require('chai')
 
 
 // Test value
-middle([1, 2, 3, 4, 5]); 
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3])
+// middle([1, 2, 3, 4, 5]); 
+// assertArraysEqual(middle([1, 2, 3, 4, 5]),[3])
+
+describe("#middle", () => {
+  it("should give middle value", () => {
+    const array = [1, 2, 3, 4, 5];
+    assert.deepEqual(middle(array),[3]);
+  })
+})
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,20 @@
 const tail = require('../tail');
 const assertEqual = require('../assertEqual');
+const { assert } = require('chai');
 
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 console.log(assertEqual(result.length, 2));
+
 assertEqual(result[0],"Lighthouse");
 assertEqual(result[1],"Labs");
+
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
 assertEqual(words.length, 3);
+
+describe("#tail",() => {
+  it("should return the length of the array", () => {
+    assert.deepEqual(tail(["Hello","Lighthouse", "Labs"]),["Lighthouse","Labs"])
+  })
+})
